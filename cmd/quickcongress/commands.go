cd42d3b44fda3fbfd2f1881fd43e3276e7df4e56
zfoteff <zfoteff@protonmail.com>
2023-06-09 14:30:34 -0700

    update

--- cmd/quickcongress/commands.go
+++ cmd/quickcongress/commands.go
@@ -1,16 +1,21 @@
 package quickcongress
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/zfoteff/quick-congress/bin"
+)
+
+func showMenu(cmd *cobra.Command, args []string) {
+	println(bin.MenuString)
+}
 
 func congressMenu() *cobra.Command {
-	var userIn int
+	// var userIn int
 
 	var command *cobra.Command = &cobra.Command{
 		Use:   "congress",
 		Short: "Get information about a session of congress",
-		Run: func(cmd *cobra.Command, args []string) {
-
-		},
+		Run:   showMenu,
 	}
 
 	return command
