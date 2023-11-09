a64bd389fe0bfc64d7e096210fea0c633cdf48db
zfoteff <zfoteff@zagmail.gonzaga.edu>
2023-11-09 09:03:57 -0800

    update

--- pkg/quickcongress/node/bill-node.go
+++ pkg/quickcongress/node/bill-node.go
@@ -9,31 +9,35 @@ import (
 	"github.com/zfoteff/quick-congress/pkg/quickcongress/controller/cli"
 )
 
-type CongressNode struct {
+type BillNode struct {
 	MenuNode
 	cli.CLIInterface
 }
 
-// Create a new congress node that can be evaluated
-func NewCongressNode(prev Node) *CongressNode {
-	return &CongressNode{
+// Create a new bill node that can be evaluated
+func NewBillNode(prev *Node) *BillNode {
+	return &BillNode{
 		MenuNode{
-			Text:       bin.CongressMenu,
+			Text:       bin.BillMenu,
 			StartRange: 0,
 			EndRange:   2,
-			Previous:   &prev,
+			Previous:   prev,
 		}, cli.CLIInterface{},
 	}
 }
 
-func (c *CongressNode) GetNodeInput() int16 {
+func (n *BillNode) GetBillByNumber() *BillNode {
+	return nil
+}
+
+func (n *BillNode) GetNodeInput() int16 {
 	var menuChoice string
 
 	for {
-		fmt.Print(c.Text)
+		fmt.Print(n.Text)
 		fmt.Scanln(&menuChoice)
 
-		//	Check if user inputted a quit command, or a backup command
+		// Check if user inputted a quit command, or a backup command
 		switch strings.ToLower(menuChoice) {
 		case "q":
 			// quit
@@ -55,25 +59,17 @@ func (c *CongressNode) GetNodeInput() int16 {
 
 // Evaluation function for the congress menu node. Should output to the
 // terminal if a command node is reached
-func (c *CongressNode) Evaluate() Node {
-	var nextNode Node
+func (n *BillNode) Evaluate() Node {
+	var nextNode *Node
 
-	switch c.GetNodeInput() {
+	switch n.GetNodeInput() {
 	case -2:
-		nextNode = *c.Previous
+		nextNode = n.Previous
 	case -1:
 		nextNode = nil
 	case 0:
-		// Get current congress session and set next node as main menu
-		println(c.GetCurrentCongressSession())
-		nextNode = NewHeadMenuNode()
-	case 1:
-		// Get details about a past session of congress
-		nextNode = NewSessionNode(c)
-	case 2:
-		// get range of congress sessions
-		println("2")
+		nextNode = nil
 	}
 
-	return nextNode
+	return *nextNode
 }
